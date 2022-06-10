040f5c9414ffaa9e5f6ff6f4e4b171617025853a
Isaias Eduardo Castañeda Diaz <cisaias9904@gmail.com>
2022-06-10 16:26:38 -0500

    logic changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_URL = "https://rickandmortyapi.com/api/character"
 
-const datainfo = document.querySelector("#data_main")
+const data_main = document.querySelector("#data_main")
 
 
 const getCharacters = async (url) => {
@@ -9,26 +9,41 @@ const getCharacters = async (url) => {
     const data = await resp.json();
 
     console.log(data.results)
-
+    let characters = []
     data.results.map((element) => {
 
-        let content = `
-<article class="main_section_data">
-            <section class="section_photo">
-        <img src="${element.image}" alt="Imagen de personaje">
-        <span> ${element.status}</span>
-    </section>
-    <section class="section_info">
-        <h4>${element.name}</h4>
-        <h3>${element.origin.name}</h5>
-        <h5>${element.species}</h5>
-    </section>
-    </article>
-`
-        datainfo.innerHTML += content;
+        const main_article = document.createElement("article")
+        main_article.classList.add("main_section_data_character")
+        const first_section = document.createElement("section")
+        first_section.classList.add("section_photo")
+        const img = document.createElement("img")
+        img.src = element.image;
+
+        const span = document.createElement("span")
+        element.status === "Alive" ?
+            span.classList.add("span_alive") : element.status === "Dead" ? span.classList.add("span_dead") : span.classList.add("span_unknown")
+
+        span.textContent = element.status
+
+        first_section.append(img, span)
+
+
+        const second_section = document.createElement("section")
+        second_section.classList.add("section_info")
+        const h4 = document.createElement("h4")
+        h4.textContent = element.name
+        const h3 = document.createElement("h3")
+        h3.textContent = element.origin.name
+        const h5 = document.createElement("h5")
+        h5.textContent = element.species
+        second_section.append(h4, h3, h5)
+
+        main_article.append(first_section, second_section)
+
+        characters.push(main_article)
     });
 
-
+    data_main.append(...characters)
 }
 
 getCharacters(API_URL)
